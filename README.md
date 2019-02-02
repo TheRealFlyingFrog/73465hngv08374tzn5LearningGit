01b1eed030028471ee4e09536c2dc79ea5224c02
46624323 <Aktionautgmail.com>
2019-02-02 11:26:45 +0100

    My first commit dawg

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 73465hngv08374tzn5LearningGit
 Just me getting familiar with git
+
+Your welcome to fork it, but i really wouldnt5 see why haha
+Nothing to see here
---
+++ StockClass.py
@@ -0,0 +1,39 @@
+import datetime
+
+class Stock():
+
+    owned = ""
+
+    def __init__(self,name,price,country,ranking,size):
+        self.name    = name
+        self.price   = price
+        self.country = country
+        self.ranking = ranking
+        self.size    = size
+
+    def stock_info(self):
+        print("Name:    {0}".format(self.name))
+        print("Price:   {0}".format(self.price))
+        print("Country: {0}".format(self.country))
+        print("Ranking: {0}".format(self.ranking))
+        print("Size:    {0}".format(self.size))
+
+    def buy_stock(self,shares,name,price):
+        #get current date and time
+        now = datetime.datetime.now()
+        print("You bought {0} Shares of {1} at {2} - {3}".format(shares,name,price,str(now) ))
+       
+        self.owned = shares
+        
+    def sell_stock(self,shares,name,price):
+        #get current date and time
+        now = datetime.datetime.now()
+        
+        if (shares > self.owned):
+            print("You dont own that many shares.")
+        else:
+            print("You sold {0} Shares of {1} at {2} - {3}".format(shares,name,price,str(now) ))
+            print("You have {0} left".format(int(shares)-int(self.owned)))
+            
+
+
